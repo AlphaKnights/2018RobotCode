380c03412b4776168675b4933c56e9cd56e1260d
Michael Maddy <MichaelMvv@users.noreply.github.com>
2018-01-23 08:40:51 -0800

    Hi

--- src/org/usfirst/frc/team6695/robot/AlphaMDrive.java
+++ src/org/usfirst/frc/team6695/robot/AlphaMDrive.java
@@ -12,42 +12,43 @@ public class AlphaMDrive extends RobotDriveBase {
 	TalonSRX rearLeft;
 	TalonSRX frontRight;
 	TalonSRX rearRight;
-	
+
 	ControlMode cm;
 
-	public AlphaMDrive(TalonSRX frontLeftMotor, TalonSRX rearLeftMotor, TalonSRX frontRightMotor, TalonSRX rearRightMotor, ControlMode mode) {
+	public AlphaMDrive(TalonSRX frontLeftMotor, TalonSRX rearLeftMotor, TalonSRX frontRightMotor,
+			TalonSRX rearRightMotor, ControlMode mode) {
 		frontLeft = frontLeftMotor;
 		frontRight = frontRightMotor;
 		rearLeft = rearLeftMotor;
 		rearRight = rearRightMotor;
 		cm = mode;
 	}
-	
+
 	public void driveCartesian(double ySpeed, double xSpeed, double zRotation, double gyroAngle, double throttle) {
-	    ySpeed = limit(ySpeed);
-	    ySpeed = applyDeadband(ySpeed, m_deadband);
+		ySpeed = limit(ySpeed);
+		ySpeed = applyDeadband(ySpeed, m_deadband);
 
-	    xSpeed = limit(xSpeed);
-	    xSpeed = applyDeadband(xSpeed, m_deadband);
+		xSpeed = limit(xSpeed);
+		xSpeed = applyDeadband(xSpeed, m_deadband);
 
-	    // Compensate for gyro angle.
-	    Vector2d input = new Vector2d(ySpeed, xSpeed);
-	    input.rotate(-gyroAngle);
+		// Compensate for gyro angle.
+		Vector2d input = new Vector2d(ySpeed, xSpeed);
+		input.rotate(-gyroAngle);
 
-	    double[] wheelSpeeds = new double[4];
-	    wheelSpeeds[MotorType.kFrontLeft.value] = input.x + input.y + zRotation;
-	    wheelSpeeds[MotorType.kFrontRight.value] = input.x - input.y + zRotation;
-	    wheelSpeeds[MotorType.kRearLeft.value] = -input.x + input.y + zRotation;
-	    wheelSpeeds[MotorType.kRearRight.value] = -input.x - input.y + zRotation;
+		double[] wheelSpeeds = new double[4];
+		wheelSpeeds[MotorType.kFrontLeft.value] = input.x + input.y + zRotation;
+		wheelSpeeds[MotorType.kFrontRight.value] = input.x - input.y + zRotation;
+		wheelSpeeds[MotorType.kRearLeft.value] = -input.x + input.y + zRotation;
+		wheelSpeeds[MotorType.kRearRight.value] = -input.x - input.y + zRotation;
 
-	    normalize(wheelSpeeds);
-	    throttle=(throttle +1 ) / 2;
-	    frontLeft.set(cm, wheelSpeeds[MotorType.kFrontLeft.value] * m_maxOutput * throttle);
-	    frontRight.set(cm, wheelSpeeds[MotorType.kFrontRight.value] * m_maxOutput * throttle);
-	    rearLeft.set(cm, wheelSpeeds[MotorType.kRearLeft.value] * m_maxOutput * throttle);
-	    rearRight.set(cm, wheelSpeeds[MotorType.kRearRight.value] * m_maxOutput * throttle);
+		normalize(wheelSpeeds);
+		throttle = 1 - ((throttle + 1) / 2);
+		frontLeft.set(cm, wheelSpeeds[MotorType.kFrontLeft.value] * m_maxOutput * throttle);
+		frontRight.set(cm, wheelSpeeds[MotorType.kFrontRight.value] * m_maxOutput * throttle);
+		rearLeft.set(cm, wheelSpeeds[MotorType.kRearLeft.value] * m_maxOutput * throttle);
+		rearRight.set(cm, wheelSpeeds[MotorType.kRearRight.value] * m_maxOutput * throttle);
 
-	    m_safetyHelper.feed();
+		m_safetyHelper.feed();
 	}
 
 	@Override
@@ -58,9 +59,9 @@ public class AlphaMDrive extends RobotDriveBase {
 	@Override
 	public void stopMotor() {
 		frontLeft.set(cm, 0);
-	    frontRight.set(cm, 0);
-	    rearLeft.set(cm, 0);
-	    rearRight.set(cm, 0);
+		frontRight.set(cm, 0);
+		rearLeft.set(cm, 0);
+		rearRight.set(cm, 0);
 	}
 
 	@Override
--- src/org/usfirst/frc/team6695/robot/Robot.java
+++ src/org/usfirst/frc/team6695/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package org.usfirst.frc.team6695.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
@@ -12,10 +5,6 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 
-/**
- * This is a demo program showing how to use Mecanum control with the RobotDrive
- * class.
- */
 public class Robot extends IterativeRobot {
 	private static final int kFrontLeftChannel = 1;
 	private static final int kRearLeftChannel = 2;
@@ -34,11 +23,6 @@ public class Robot extends IterativeRobot {
 		TalonSRX frontRight = new TalonSRX(kFrontRightChannel);
 		TalonSRX rearRight = new TalonSRX(kRearRightChannel);
 
-		// Invert the left side motors.
-		// You may need to change or remove this to match your robot.
-		// frontLeft.setInverted(true);
-		// rearLeft.setInverted(true);
-
 		driveTrain = new AlphaMDrive(frontLeft, rearLeft, frontRight, rearRight, ControlMode.PercentOutput);
 		driveTrain.setDeadband(.01);
 		joystick = new Joystick(kJoystickChannel);
@@ -46,8 +30,6 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void teleopPeriodic() {
-		// Use the joystick X axis for lateral movement, Y axis for forward
-		// movement, and Z axis for rotation.
 		driveTrain.driveCartesian(joystick.getX(), joystick.getY(), joystick.getZ(), 0.0, joystick.getThrottle());
 	}
 }
